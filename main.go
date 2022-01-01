f695c3e6a033705b803f042dcf0a07a86a9a7e48
machamp0714 <rateliasudaisuki@gmail.com>
2022-01-01 15:51:19 +0900

    create: TimeCrowd Provider

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"terraform-provider-timecrowd/timecrowd"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return timecrowd.Provider()
+		},
+	})
 }
--- timecrowd/provider.go
+++ timecrowd/provider.go
@@ -1,1 +1,12 @@
 package timecrowd
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap:   map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{},
+	}
+}
